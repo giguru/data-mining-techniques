f7f70a692d50f2e2293835aef98a4bdf3e63b478
Giguru Scheuer <giguru@uw-s.nl>
2022-04-05 21:05:15 +0200

    Plot all graphs to understand data

--- data_understanding.py
+++ data_understanding.py
@@ -1,37 +1,64 @@
 from typing import List
 from pandas import DataFrame
 from matplotlib import pyplot as plt
-from utils import read_data
+from utils import read_data, get_subset_by_variable
 import numpy as np
 
 
-def print_values_bar(variableName: str, df: DataFrame):
+def print_values_bar(variable_name: str, df: DataFrame):
     """
-    :param variableName: Variable name of the variable column in the CSV
+    :param variable_name: Variable name of the variable column in the CSV
     :param df:
     """
-    subset = df[df['variable'] == variableName]
+    subset = get_subset_by_variable(variable_name, df)
     count_dict = subset['value'].value_counts().to_dict()
     plt.bar(
         list(count_dict.keys()),
         list(count_dict.values()),
     )
-    plt.title(f"Values of '{variableName}'")
+    plt.title(f"Values of '{variable_name}'")
     plt.show()
 
 
-def print_values_hist(variableName: str, df: DataFrame, bins: List):
+def print_values_hist(variable_name: str, df: DataFrame, bins: List):
     """
-    :param variableName: Variable name of the variable column in the CSV
+    :param variable_name: Variable name of the variable column in the CSV
     :param df:
     """
-    subset = df[df['variable'] == variableName]
+    subset = get_subset_by_variable(variable_name, df)
     plt.hist(x=subset['value'].to_list(), bins=bins)
-    plt.title(f"Values of '{variableName}'")
+    plt.title(f"Values of '{variable_name}'")
     plt.show()
 
 
-print_values_bar('mood', read_data())
-print_values_bar('circumplex.arousal', read_data())
-print_values_bar('circumplex.valence', read_data())
-print_values_hist('activity', read_data(), list(np.linspace(0, 1, 20)))
+data = read_data()
+print_values_bar('mood', data)
+print_values_bar('circumplex.arousal', data)
+print_values_bar('circumplex.valence', data)
+print_values_hist('activity', data, list(np.linspace(0, 1, 20)))
+
+# The range of screen time is variable
+variables_with_unfixed_range = [
+    'screen',
+    'appCat.builtin',
+    'appCat.communication',
+    'appCat.entertainment',
+    'appCat.finance',
+    'appCat.game',
+    'appCat.office',
+    'appCat.other',
+    'appCat.social',
+    'appCat.travel',
+    'appCat.unknown',
+    'appCat.utilities',
+    'appCat.weather'
+]
+for variable_name in variables_with_unfixed_range:
+    variable_subset = get_subset_by_variable('screen', data)['value']
+    min_value = min(variable_subset)
+    max_value = max(variable_subset)
+
+    print_values_hist(variable_name, data, list(np.linspace(min_value, max_value, 100)))
+
+print(f"Occurences of variable='call': {len(get_subset_by_variable('call', data))}")
+print(f"Occurences of variable='sms': {len(get_subset_by_variable('sms', data))}")
--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
 import pandas
+from pandas import DataFrame
 
 
 def read_data(**kwargs):
     return pandas.read_csv('dataset_mood_smartphone.csv', **kwargs)
+
+
+def get_subset_by_variable(variable_name: str, df: DataFrame):
+    return df[df['variable'] == variable_name]
