483d38309bd221e52c8cbfb62a7199144c4fc1bb
Vincenzo Scopelliti <vi@Vincenzos-MacBook-Pro.local>
2022-04-06 19:24:10 +0200

    added time dependence exploration of mood. plots  mean mood vs date, time of day and day  of week

--- data_understanding.py
+++ data_understanding.py
@@ -3,6 +3,19 @@ from pandas import DataFrame
 from matplotlib import pyplot as plt
 from utils import read_data, get_subset_by_variable
 import numpy as np
+from pandas.api.types import is_datetime64_any_dtype as is_datetime
+import os
+
+OUTPUT_PATH = './output/'
+
+def check_existing_folder(this_path):
+    MYDIR = (this_path)
+    CHECK_FOLDER = os.path.isdir(MYDIR)
+
+    # If folder doesn't exist, then create it.
+    if not CHECK_FOLDER:
+        os.makedirs(MYDIR)
+        print("created folder : ", MYDIR)
 
 
 def print_values_bar(variable_name: str, df: DataFrame):
@@ -30,6 +43,31 @@ def print_values_hist(variable_name: str, df: DataFrame, bins: List):
     plt.title(f"Values of '{variable_name}'")
     plt.show()
 
+
+def plot_temporal_effects(df: DataFrame, time_window: str, variable_name: str):
+    """
+    :param df: DataFrame with time as index and variable_name as value.
+                Example: t=0,1,...,24 (hour)
+                         mood = 2,3,...,6
+    :param time_window: label of horizontal axis. time window considered.
+                        E.g. hour  (during the day), date,...
+    :param variable_name: mood, or any other attribute
+    :return: Plots  the time series
+    """
+    this_path = os.path.join(OUTPUT_PATH, 'temporal_dependency')
+    check_existing_folder(this_path)
+    plt.plot(df.index,
+             df,
+             label=time_window
+    )
+    plt.title(f"Time evolution mean of '{variable_name}'")
+    plt.xlabel(time_window)
+    plt.ylabel(variable_name)
+    plt.xticks(rotation=45)
+    plt.show()
+    plt.savefig(os.path.join(this_path, f'{time_window}.pdf'))
+    plt.tight_layout()
+    plt.close()
 
 data = read_data()
 print_values_bar('mood', data)
@@ -62,3 +100,22 @@ for variable_name in variables_with_unfixed_range:
 
 print(f"Occurences of variable='call': {len(get_subset_by_variable('call', data))}")
 print(f"Occurences of variable='sms': {len(get_subset_by_variable('sms', data))}")
+
+# check periodicity
+data['hour'] = data.time.dt.hour
+data['date'] = data.time.dt.date
+data['weekday'] = data.time.dt.strftime('%A')
+data['month'] = data.time.dt.month
+data['year'] = data.time.dt.year
+
+item = 'mood'
+
+def skipna_mean(df):
+    return df.mean(skipna=True)
+cats = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+for time_window  in  ['hour', 'date', 'weekday', 'month']:
+    this_mean = data.loc[data['variable'] == item].groupby(by=[time_window])['value'].agg(skipna_mean)
+    if time_window=='weekday':
+        this_mean = this_mean.reindex(cats)
+    plot_temporal_effects(this_mean, time_window,  'mood')
