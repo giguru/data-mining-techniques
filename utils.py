2e96aff72a42c77132138f7e08f20c779ee6d6ff
Giguru Scheuer <giguru@uw-s.nl>
2022-04-08 18:21:47 +0200

    Create feature matrix

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
+from collections import defaultdict
+
 import pandas as pd
 import numpy as np
 from pandas import DataFrame
-from typing import List, Dict
+from typing import List, Dict, Callable
 from tqdm import tqdm
 
 
@@ -39,35 +41,46 @@ def get_subset_by_variable(variable_name: str, df: DataFrame):
     return df[df['variable'] == variable_name]
 
 
-def get_temporal_records(df: DataFrame, history: int, aggregation_actions: Dict[str, str] = None):
+def aggregate_actions_per_user_per_day(df: DataFrame, variable_key, agg_func):
+    df_only_variable = df[df['variable'] == variable_key]
+    df_aggregate_variable_per_date_and_user = df_only_variable.groupby(
+        [df_only_variable['time'].dt.date, df_only_variable['id']]
+    ).agg({
+        'value': agg_func,
+        'timestamp': 'max',
+        'time': 'max',
+        'id': 'first',  # Retain the value
+        'variable': 'first'  # Retain the value
+    })
+
+    # Data without the variable column
+    df_without_variable = df[df['variable'] != variable_key]
+    df = pd.concat([df_aggregate_variable_per_date_and_user, df_without_variable])
+    return df, df_aggregate_variable_per_date_and_user
+
+
+def get_temporal_records(df: DataFrame,
+                         history: int,
+                         aggregation_actions_per_user_per_day: Dict[str, str] = None,
+                         aggregation_actions_total_history: Dict[str, Callable] = None
+                         ):
     """
 
     :param df:
     :param history: In seconds
-    :param aggregation_actions:
+    :param aggregation_actions_per_user_per_day:
+    :param aggregation_actions_total_history:
     :return:
     """
-    if aggregation_actions is None:
-        aggregation_actions = {}
+    if aggregation_actions_per_user_per_day is None:
+        aggregation_actions_per_user_per_day = {}
 
-    # The mean must be always average
-    aggregation_actions['mood'] = 'mean'
+    # The mean must be always average per day
+    aggregation_actions_per_user_per_day['mood'] = 'mean'
 
-    for variable_key, agg_func in aggregation_actions.items():
-        df_only_variable = df[df['variable'] == variable_key]
-        df_aggregate_variable_per_date_and_user = df_only_variable.groupby(
-            [df_only_variable['time'].dt.date, df_only_variable['id']]
-        ).agg({
-            'value': agg_func,
-            'timestamp': 'max',
-            'time': 'max',
-            'id': 'first',  # Retain the value
-            'variable': 'first'  # Retain the value
-        })
-
-        # Data without the variable column
-        df_without_variable = df[df['variable'] != variable_key]
-        df = pd.concat([df_aggregate_variable_per_date_and_user, df_without_variable])
+    # some variable keys you would like to have average per day
+    for variable_key, agg_func in aggregation_actions_per_user_per_day.items():
+        df, _ = aggregate_actions_per_user_per_day(df, variable_key, agg_func)
 
     # Now create one data frame with the mean mood data and the non-mood data
     sorted_df = df.sort_values(by=['timestamp'])
@@ -86,15 +99,29 @@ def get_temporal_records(df: DataFrame, history: int, aggregation_actions: Dict[
                     # Since the list is sorted by time, if you reach an item in the
                     # frame, you can assume the rest is also in the frame.
                     break
+
             if first_index_in_frame > 0:
                 running_window = running_window[first_index_in_frame:]
 
-            records.append([
+            # Other variables you would like to have aggregated over the entire time period
+            if aggregation_actions_total_history is None:
                 # Only append data of the user, but remove the ID.
-                # The ID is irrelevant for any machine learning model.
-                [r.drop(['id']).to_dict() for r in running_window if r['id'] == row['id']],
-                # the target
-                row['value']
+                # The ID is irrelevant for every machine learning model.
+                features = [r.drop('id') for r in running_window if r['id'] == row['id']]
+            else:
+                features = defaultdict(list)
+                for r in running_window:
+                    if r['id'] == row['id'] and r['variable'] in aggregation_actions_total_history:
+                        features[r['variable']].append(r['value'])
+
+                features = dict(features)
+                for variable_key, variable_values in features.items():
+                    agg_func = aggregation_actions_total_history[variable_key]
+                    features[variable_key] = agg_func(variable_values)
+
+            records.append([
+                features,
+                row['value']  # the target
             ])
 
         running_window.append(row)
